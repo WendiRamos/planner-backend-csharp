104471aeb2e673d1bd4e78c6f68d242000495267
Wendi Ramos <81653845+WendiRamos@users.noreply.github.com>
2024-08-27 21:33:26 -0300

    feat: refatorando codigo

--- src/Journey.Infrastructure/JourneyDbContext.cs
+++ src/Journey.Infrastructure/JourneyDbContext.cs
@@ -7,16 +7,11 @@ namespace Journey.Infrastructure
     public class JourneyDbContext : DbContext
     {
         public DbSet<Trip>Trips { get; set; }
+        public DbSet<Activity>Activities { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Data Source=..\\..\\database\\JourneyDatabase.db");
-        }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Activity>().ToTable("Activities");
         }
     }
 }
